f3ac5ca981f40def8ed880e6eaecf4c59a765868
Michael Scriven <bigscriv03@gmail.com>
2015-02-03 17:20:06 -0500

    Renamed views to haml files

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,8 +3,18 @@ class PinsController < ApplicationController
   end
 
   def new
+    @pin = Pin.new
   end
 
   def show
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+  end
+
+  private
+  def pin_params
+    params.require(:pin).permit(:title, :description)
   end
 end
